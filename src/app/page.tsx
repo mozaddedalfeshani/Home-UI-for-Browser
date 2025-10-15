dd821d9e966980c2c2deeb6ccd01d8abf8dee8d7
mozaddedalfeshani <murad15-6047@diu.edu.bd>
2025-10-16 02:01:13 +0600

    Add Radix UI components and enhance TabsZone layout with styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-black min-h-screen w-full p-6">
-      <div className="flex flex-row ">
+    <div className="bg-black min-h-screen w-full ">
+      <div className="flex flex-row min-h-screen p-6">
         {/* Left side  */}
-        <div>
+        <div className="w-3/4">
           <DigitalClock />
           <TabsZone />
         </div>
         {/* Right side */}
-        <div></div>
+        <div className="w-1/4 bg-blue-950">This is right side</div>
       </div>
     </div>
   );
--- src/components/Home/TabsZone/index.tsx
+++ src/components/Home/TabsZone/index.tsx
@@ -1,7 +1,53 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 import React from "react";
 
 const TabsZone = () => {
-  return <div>TabsZone</div>;
+  return (
+    <div>
+      <Card>
+        <div className="w-full bg-white h-10 flex flex-row justify-end items-center px-2">
+          <Button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none">
+              <g clipPath="url(#clip0_4418_9827)">
+                <path
+                  d="M8 12H16"
+                  stroke="#000"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M12 16V8"
+                  stroke="#000"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z"
+                  stroke="#000"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_4418_9827">
+                  <rect width="24" height="24" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+          </Button>
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 export default TabsZone;
