7dcc5303bd311211d3e485d31a4c0636070d5104
mozaddedalfeshani <murad15-6047@diu.edu.bd>
2026-04-15 16:54:08 +0600

    feat: add Toaster component for improved notification display

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Share_Tech_Mono, Fredoka } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const shareTech = Share_Tech_Mono({
   subsets: ["latin"],
@@ -39,6 +40,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange>
             {children}
+            <Toaster />
           </ThemeProvider>
         </body>
       </html>
